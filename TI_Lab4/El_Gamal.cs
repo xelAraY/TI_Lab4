174ab48ed159245cc08c5b75834c42c12bf931d7
xelAraY <alexey.yarohovich18@gmail.com>
2022-05-03 12:21:49 +0300

    Добавьте файлы проекта.

---
+++ TI_Lab4/El_Gamal.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace TI_Lab4
+{
+    public class El_Gamal
+    {
+        private int Q { get; set; }
+        private int P { get; set; }
+        private int H { get; set; }
+        private int X { get; set; }
+        private int K { get; set; }
+        private int G { get; set; }
+        private int Y { get; set; }
+        public int R { get; set; }
+        public int V { get; set; }
+        private byte[] data { get; set; }
+
+        public El_Gamal(int q, int p, int h, int x, int k, byte[] data)
+        {
+            if(IsPrime(q))
+                Q = q;
+            else
+                throw new Exception("Число q должно быть простое");
+
+            if(IsPrime(p) && (p-1)%q == 0)
+                P = p;
+            else 
+                throw new Exception("Число p должно является делителем p-1 и быть простым");
+
+            if (h > 1 && h < p - 1)
+            {
+                int g = FastExponentiation(p, h, (p - 1) / q);
+                if (g > 1)
+                {
+                    G = g;
+                    H = h;
+                }
+                else
+                    throw new Exception("Число g должно быть больше 1, выберите другое h");
+            }
+            else
+                throw new Exception("Число h должно быть больше 1 и меньше p-1");
+
+            if(x > 0 && x < q)
+                X = x;
+            else 
+                throw new Exception("Число x должно быть больше 0 и меньше q");
+
+            if (k > 0 && k < q)
+                K = k;
+            else
+                throw new Exception("Число k должно быть больше 0 и меньше q");
+
+            this.data = data;
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number == 1 || number == 2)
+                return true;
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private int FastExponentiation(BigInteger mod, BigInteger num, BigInteger deg)
+        {
+            BigInteger y = 1;
+            while (deg != 0)
+            {
+                while (deg % 2 == 0)
+                {
+                    deg /= 2;
+                    num = (num * num) % mod;
+                }
+                deg--;
+                y = (y * num) % mod;
+            }
+            return (int)y;
+        }
+
+        public (int, int, int) SignatureMessage()
+        {
+            int y = FastExponentiation(P, G, X);
+            Y = y;
+
+            int hash = 100;
+            foreach(byte b in data)
+            {
+                hash = FastExponentiation(Q, hash + b, 2);   
+            }
+
+            int r = FastExponentiation(Q, FastExponentiation(P, G, K), 1);
+            R = r;
+            int s = FastExponentiation(Q, FastExponentiation(Q, K, Q-2)*FastExponentiation(Q, hash + X*r, 1), 1);
+
+            return (hash, r, s);
+        }
+
+        public bool CheckSignature(byte[] data, int r, int s)
+        {
+            int hash = 100;
+            foreach (byte b in data)
+            {
+                hash = FastExponentiation(Q, hash + b, 2);
+            }
+
+            int w = FastExponentiation(Q, s, Q - 2);
+            int u1 = FastExponentiation(Q, FastExponentiation(Q, hash, 1) * FastExponentiation(Q, w, 1), 1);
+            int u2 = FastExponentiation(Q, FastExponentiation(Q, r, 1) * FastExponentiation(Q, w, 1), 1);
+            int v = FastExponentiation(Q, FastExponentiation(P, FastExponentiation(P, G, u1)*FastExponentiation(P, Y, u2), 1), 1);
+            V = v;
+
+            if (v == r)
+                return true;
+            else 
+                return false;
+        } 
+    }
+}
---
+++ TI_Lab4/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using Microsoft.Win32;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TI_Lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string _filePath = String.Empty;
+
+        private static El_Gamal elGamal;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignFile_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                _filePath = ofd.FileName;
+                byte[] data = File.ReadAllBytes(ofd.FileName);
+                try
+                {
+                    elGamal = new El_Gamal(int.Parse(PrimeNumQ_TextBox.Text),
+                        int.Parse(PrimeNumP_TextBox.Text), int.Parse(h_TextBox.Text),
+                        int.Parse(x_TextBox.Text), int.Parse(k_TextBox.Text), data);
+
+                    //elGamal = new El_Gamal(593, 3559, 3, 17, 23, data);
+
+                    (int hash, int r, int s) signature = elGamal.SignatureMessage();
+
+                    if(signature.r == 0 || signature.s == 0)
+                    {
+                        throw new Exception("Выберите другое значение для k");
+                    }
+
+                    Hash_TextBox.Text = Convert.ToString(signature.hash, 10);
+
+                    string? str;
+                    using (FileStream fs = new FileStream(_filePath, FileMode.Open, FileAccess.Read))
+                    using (StreamReader sw = new StreamReader(fs))
+                    {
+                        str = sw.ReadToEnd();
+                    }
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(str);
+                    sb.Append($"\nr = {signature.r}, s = {signature.s};\n");
+                    SignatureText_TextBox.Text = $"r = {signature.r}, s = {signature.s}";
+
+                    string fileName = Path.GetDirectoryName(_filePath) + '\\' + "New_" +
+                            Path.GetFileName(_filePath);
+                    using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.Write(sb.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }                 
+            }
+        }
+
+        private void CheckSignature_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                _filePath = ofd.FileName;               
+                byte[] data = File.ReadAllBytes(ofd.FileName);         
+
+                //string pattern = "\\nr = [0-9]+, s = [0-9]+;\\n";
+
+                var message = Encoding.ASCII.GetString(data);
+
+                //var rsString = Regex.Matches(message, "\\nr = [0-9]+, s = [0-9]+;\\n").ToString();
+                var mathes = Regex.Matches(message, "[0-9]+");
+
+                if (mathes.Count > 1)
+                {
+                    int r = int.Parse(mathes[0].ToString());
+                    int s = int.Parse(mathes[1].ToString());
+
+                    StringBuilder sb = new StringBuilder();
+                    string newMessage = Regex.Replace(message, "\\nr = [0-9]+, s = [0-9]+;\\n", string.Empty);
+
+                    data = Encoding.ASCII.GetBytes(newMessage);
+
+                    if (elGamal.CheckSignature(data, r, s))
+                        sb.Append($"Подпись является подлинной, r = v ({r} = {elGamal.V})");
+                    else
+                        sb.Append($"Подпись не является подлинной, r ≠ v ({r} ≠ {elGamal.V})");
+
+                    CheckSignatureText_TextBox.Text = sb.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Файл не подписан");
+                    return;
+                }                                            
+            }
+        }
+    }
+}
